31488b81b9fd078b9044d23346ff9a1ca470fd1c
sudrien <sudrien@seventeenteen.sudrien.net>
2025-01-16 00:51:08 -0500

    It still scrolls vertically for no good reason

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'supermarket-adder-cache-v1';
+const urlsToCache = [
+  '/',
+  '/d3-geo-projection.v4.js',
+  '/d3.v6.js'
+];
+
+// Install event: Cache specified resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event: Network First with Cache Fallback
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If the response is valid, update the cache and return the response
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      })
+      .catch(() => {
+        // If the network request fails, try to return the cached version
+        return caches.match(event.request);
+      })
+  );
+});
